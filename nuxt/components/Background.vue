fc9fb67edf685c47dc871fa112abfc1c38937ce6
Matt Wiggins <matt@consumeandcreate.co>
2025-11-12 15:02:39 -0700

    Scroll snap infinite first pass.

---
+++ nuxt/components/Background.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="placeholder">
+    <div class="card">
+      <p>{{ title }}</p>
+    </div>
+  </section>
+</template>
+
+<script setup>
+// Props
+const props = defineProps({
+  title: {
+    type: String,
+    required: false
+  }
+});
+</script>
+
+<style lang='scss'>
+.placeholder {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  scroll-snap-align: start;
+  scroll-snap-stop: always;
+
+  .card {
+    position: relative;
+    margin: span(1);
+    box-shadow: 0px 0px 0px 1px $gray;
+    margin: span(1);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-grow: 1;
+  }
+}
+
+</style>
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,10 +1,63 @@
 <template>
   <div class="page">
-    <Placeholder title="LOGO EXPLOSION (Intro Message)" />
-    <Placeholder title="Who We Are" />
-    <Placeholder title="What We Did" />
-    <Placeholder title="Awards We Won" />
-    <Placeholder title="Here We Are" />
-    <Placeholder title="LOGO EXPLOSION (Contact Message)" />
+    <div class="slides" ref="slides">
+      <Placeholder title="LOGO EXPLOSION (intro/contact)" />
+      <Placeholder title="Who We Are" />
+      <Placeholder title="What We Did" />
+      <Placeholder title="Awards We Won" />
+      <Placeholder title="Here We Are" />
+    </div>
+    <div class="clones" ref="clones">
+      <Placeholder title="LOGO EXPLOSION (intro/contact)" />
+      <Placeholder title="Who We Are" />
+      <Placeholder title="What We Did" />
+      <Placeholder title="Awards We Won" />
+      <Placeholder title="Here We Are" />
+    </div>
   </div>
 </template>
+
+<script setup>
+const slides = ref(null);
+const clones = ref(null);
+
+// Mounted
+onMounted(() => {
+  document.addEventListener('scrollsnapchange', onScrollSnapChange);
+});
+
+// Before Unmount
+onBeforeUnmount(() => {
+  window.removeEventListener('scrollsnapchange', onScrollSnapChange);
+});
+
+// Methods
+function scroll(y) {
+  window.scrollTo({
+    'top': y,
+    'left': 0,
+    'behavior': 'instant'
+  });
+}
+
+function onScrollSnapChange (e) {
+  const p = e.snapTargetBlock.parentElement;
+
+  if(p === slides.value) {
+    const index = Array.from(slides.value.children).indexOf(e.snapTargetBlock);
+
+    if(index === 0) {
+      const top = clones.value.children[0].offsetTop;
+
+      // back at top, set scroll to clone top
+      scroll(top);
+    } 
+  } else if(p === clones.value) {
+    const index = Array.from(clones.value.children).indexOf(e.snapTargetBlock),
+          top = index === 0 ? clones.value.children[index].offsetTop : slides.value.children[index].offsetTop;
+
+    // in a clone, set scroll to corresponding slide
+    scroll(top);
+  }
+}
+</script>
