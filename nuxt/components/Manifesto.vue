8703ee71dd12582cc4ca5efc10652647e0422fc4
Keith Driessen <connect@hotlines.studio>
2025-11-18 07:13:00 -0700

    wip: manifesto underlines

--- nuxt/components/Manifesto.vue
+++ nuxt/components/Manifesto.vue
@@ -5,7 +5,7 @@
         <h2 class="fs-sm gray">{{ title }}</h2>
         <div class="container">
           <div class="content">
-            <h3 class="h3">We build pristine interfaces and experiences. In an era where AI can generate a website in ten minutes, we’re working with partners that still want something human. Something built and maintained by experts that pay attention to details and have a deep knowledge of how it works. For those looking to build salient touch points for their brand, let's build something meaningful!</h3>
+            <h3 class="h3">We build pristine <span class="u">interfaces</span> and <span class="u">experiences</span>. In an era where AI can generate a website in ten minutes, we’re working with partners that still want something <span class="u">human</span>. Something built and maintained by experts that pay attention to <span class="u">details</span> and have a deep knowledge of <span class="u">how it works</span>. For those looking to build salient touch points for their brand, let's build something <span class="u">meaningful</span>!</h3>
           </div>
         </div>
       </div>
